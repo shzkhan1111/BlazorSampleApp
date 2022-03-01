67b1afde6a5de237f12d76aaf349bbae4fd7a2ab
shzkhan1111 <91877371+shzkhan1111@users.noreply.github.com>
2022-03-01 16:04:35 +0500

    Add project files.

---
+++ LoginBlazor/LoginService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginBlazor.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginBlazor
+{
+    public class LoginService
+    {
+        #region Property
+        private readonly AppDbContext _appDBContext;
+        #endregion
+
+        #region Constructor
+        public LoginService(AppDbContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        #endregion
+
+        #region Get Employee by Id
+        public async Task<Employee> GetEmployeeeById(int Id)
+        {
+            Employee employee = await _appDBContext.Employees.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return employee;
+        }
+        #endregion
+        #region Login
+        public bool AuthUser(string userName , string Password)
+        {
+            //Employee employee = await _appDBContext.Employees.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            //return employee;
+            bool isAuth = _appDBContext.Employees.Any(e => e.UserName == userName && e.Password == Password);
+            return isAuth;
+        }
+        #endregion
+    }
+}
---
+++ LoginBlazor/Migrations/20220301083054_test22.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoginBlazor.Migrations
+{
+    public partial class test22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
